fa1df651aa1a76cb23cea72bbfbbece58e97aaeb
Marcin Filinger <filutekmar@gmail.com>
2018-12-28 22:07:15 +0100

    trójkąt pascala

--- kurs Java/18.10.27-28/pentle2/src/pl/sda/pentle/trójkątpascala.java
+++ kurs Java/18.10.27-28/pentle2/src/pl/sda/pentle/trójkątpascala.java
@@ -4,14 +4,17 @@ public class trójkątpascala {
 
     public static void wydruk(int n) {
 
-
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < (2 * n - 1); j++) {
-                System.out.print(" X");
+                if(j < (n - i + 5 )){
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("1 ");
+                }
+
             }
             System.out.println("");
         }
-
     }
 }
