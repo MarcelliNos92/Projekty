0c46aae49eb3a19743e8fb50683ca10a81ab112b
Marcin Filinger <filutekmar@gmail.com>
2019-01-12 13:59:54 +0100

    Zadania z programowania I

---
+++ kurs Java/18.12.15-16/silnia/src/pl/sda/struktury_danych/Main.java
@@ -0,0 +1,17 @@
+package pl.sda.struktury_danych;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        public static void silnia () {
+            Silnia silnia = new Silnia();
+            System.out.println("Silnia: " + silnia.rekurencyjnie(5));
+
+        }
+    }
+
+}
+
---
+++ kurs Java/19.01.12-13/programowanieI/src/pl/sda/podstawy/Main.java
@@ -0,0 +1,52 @@
+package pl.sda.podstawy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      Silnia s = new Silnia();
+        System.out.println(s.silnia(5));
+        System.out.println(s.ciag(5));
+        System.out.println(s.ciagFib(5));
+//        s.binarnie(5);
+//        System.out.println(s.sumacyfriteracyjnie(123));
+        s.sumacyfrrekurencyjnie(1234);
+
+        int tab[] = {1,2,3,4,5,6,7,8};
+        // 2 1 4 3
+        // int tmp = tab[1];
+        // tab[1] = tab[1+1];
+        //tab[2] = tmp;
+
+        //zamienianie elementów miejscami
+
+        for(int i = 0; i<tab.length; i++) {
+
+            int tmp = tab[i];
+            tab[i] = tab[i+1];
+            tab[i+1] = tmp;
+
+            i++;
+        }
+
+        int suma = 0;
+        int silnia = 1;
+        for(int i = 0; i<tab.length; i++) {
+
+            suma = suma + tab[i];
+            silnia = silnia*tab[i];
+        }
+        System.out.println(silnia);
+
+
+        tab[0] = 0;
+        tab[1] = 1;
+        for(int i = 2; i < tab.length; i++) {
+
+            tab[i] = tab[i-2] + tab[i-1];
+
+        }
+
+        System.out.println(tab[6]);
+    }
+}
